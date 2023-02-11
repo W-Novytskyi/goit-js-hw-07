7d0781a4744cb7b6d13ed2116102e481435f940c
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-02-11 15:17:35 +0200

    2 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,11 +26,11 @@ function onImgOriginalCard(e) {
         return
     }
     
-    console.log(e.target);
+    // console.log(e.target);
     
     const instance = basicLightbox.create(`
     <img src="${e.target.dataset.sourse}" width="800" height="600">
     `)
     
     instance.show();
-}
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const imagesList = galleryItems.map(({ preview, original, description }) => {
+    
+return `<a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>`;
+}).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', imagesList);
+
+const lightbox = new SimpleLightbox('.gallery__item', { captionsData: "alt", captionDelay: 250, });
+
